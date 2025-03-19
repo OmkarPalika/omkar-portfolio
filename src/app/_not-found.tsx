014cd02b5418d1973876d40ee186003d744a0b97
Omkar Palika <palikaomkar.22.cse@anits.edu.in>
2025-03-20 03:09:23 +0530

    Attempting Vercel Build

---
+++ src/app/_not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h2 className="text-2xl font-bold">404 - Page Not Found</h2>
+        <p>Sorry, the page you are looking for does not exist.</p>
+      </div>
+    );
+  }
+  
